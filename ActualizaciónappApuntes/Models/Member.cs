5abf791696138b03173ad5c12c96906d12587a0f
Abbyeeesss <espinosaabigail76@gmail.com>
2025-06-25 21:33:15 -0500

    cambios en las paginas creadas

--- ActualizaciónappApuntes/Models/Member.cs
+++ ActualizaciónappApuntes/Models/Member.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ActualizaciónappApuntes.Models
+public class Member
 {
-    internal class Member
-    {
-    }
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public string DeporteFav { get; set; }
+    public string Photo { get; set; }
+    public string Rol { get; set; }
+    public string Descripcion { get; set; }
+    public string SportIcon { get; set; } = "⚽";
+
 }
--- ActualizaciónappApuntes/ViewModels/AboutViewModel.cs
+++ ActualizaciónappApuntes/ViewModels/AboutViewModel.cs
@@ -1,12 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using ActualizaciónappApuntes.Models;
 
 namespace ActualizaciónappApuntes.ViewModels
 {
-    internal class AboutViewModel
+    public class AboutViewModel : INotifyPropertyChanged
     {
+        private ObservableCollection<Member> _teamMembers;
+
+        public ObservableCollection<Member> TeamMembers
+        {
+            get => _teamMembers;
+            set
+            {
+                if (_teamMembers != value)
+                {
+                    _teamMembers = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public AboutViewModel()
+        {
+            LoadTeamMembers();
+        }
+
+        private void LoadTeamMembers()
+        {
+
+            TeamMembers = new ObservableCollection<Member>
+            {
+
+                new Member
+                {
+                    Nombre = "Dana Boada",
+                    Edad = 22,
+                    DeporteFav = "Judo",
+                    Photo = "dana.png",
+                    Rol = "Diseñadora UX/UI",
+                    Descripcion = "Le apasiona crear experiencias visuales únicas y divertidas. También es una competidora dedicada de judo.",
+                    SportIcon = "🥋"
+                },
+                new Member
+                {
+                    Nombre = "Abigail Espinosa",
+                    Edad = 19,
+                    DeporteFav = "Cero Deportes",
+                    Photo = "abi.png",
+                    Rol = "Desarrolladora en FrontEnd, Arquitecta de Software",
+                    Descripcion = "Estratega en APIs y soluciones técnicas.No practica ningun deporte.",
+                    SportIcon = "🧘‍♀️"
+                },
+                new Member
+                {
+                    Nombre = "Nicolas Recalde",
+                    Edad = 20,
+                    DeporteFav = "Baloncesto",
+                    Photo = "nicolas.png",
+                    Rol = "Desarrollador Backend",
+                    Descripcion = "Apasionado por el diseño responsivo y las animaciones modernas. Fuera del código, vive para el baloncesto.",
+                    SportIcon = "🏀"
+                },
+                new Member
+                {
+                    Nombre = "Fabio Gonzalez",
+                    Edad = 21,
+                    DeporteFav = "Fútbol",
+                    Photo = "fabio.png",
+                    Rol = "Ingeniero de Datos",
+                    Descripcion = "Transforma datos en decisiones. Siempre listo para jugar un partido de fútbol o una partida de ajedrez.",
+                    SportIcon = "⚽"
+                }
+            };
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
